eb51457c3dcca6e821ad952403e6e95343c1f033
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2026-01-04 09:44:01 -0300

    feat(database): setup database file

---
+++ infra/database.js
@@ -0,0 +1,23 @@
+import { Client } from "pg";
+
+async function query(queryObj) {
+	const client = new Client({
+		host: "localhost",
+		port: 5432,
+		user: "postgres",
+		database: "postgres",
+		password: "local_password",
+	});
+
+	await client.connect();
+
+	const result = await client.query(queryObj);
+
+	await client.end();
+
+	return result;
+}
+
+export default {
+	query: query,
+};
