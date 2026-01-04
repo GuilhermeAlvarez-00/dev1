0ac8c833082854fc3484553d8315c850e3f15b09
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2026-01-04 09:44:43 -0300

    refactor(v1|status): testing request with select in database

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function whatvever(req, res) {
+import database from "../../../../../infra/database.js";
+
+async function status(req, res) {
+	const result = await database.query("SELECT 1 + 1;");
+	console.log("result", result.rows);
 	res.status(200).json({ message: "Ok não sim!" });
 }
 
-export default whatvever;
+export default status;
