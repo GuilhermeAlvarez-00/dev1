c379dc21468fbb855258a3011f39c2cc101f98c7
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2026-02-28 18:35:34 -0300

    fix(database): passing ssl enabled for production environment

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObj) {
 		database: process.env.POSTGRES_DB,
 		user: process.env.POSTGRES_USER,
 		password: process.env.POSTGRES_PASSWORD,
+		ssl: process.env.NODE_ENV === "development" ? false : true,
 	});
 	console.log("Postgres credentials: ", {
 		host: process.env.POSTGRES_HOST,
