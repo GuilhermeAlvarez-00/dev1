5f48e617e2ea8f69ab22d82250b045df34f56019
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2026-02-28 10:18:59 -0300

    refactor(database): connect to database inside trycatch block and throw error

--- infra/database.js
+++ infra/database.js
@@ -8,10 +8,17 @@ async function query(queryObj) {
 		user: process.env.POSTGRES_USER,
 		password: process.env.POSTGRES_PASSWORD,
 	});
-
-	await client.connect();
+	console.log("Postgres credentials: ", {
+		host: process.env.POSTGRES_HOST,
+		port: process.env.POSTGRES_PORT,
+		database: process.env.POSTGRES_DB,
+		user: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD,
+	});
 
 	try {
+		await client.connect();
+
 		const result = await client.query(queryObj);
 
 		await client.end();
@@ -19,6 +26,7 @@ async function query(queryObj) {
 		return result;
 	} catch (error) {
 		console.error(error);
+		throw error;
 	} finally {
 		await client.end();
 	}
